1ae284ddb13a7d3807b16ab9d0fc6ea2d54f3188
meijiao <meijiao@gmail.com>
2016-10-17 21:11:36 -0700

    Connected mongoose

--- app.js
+++ app.js
@@ -3,14 +3,22 @@ const app = express();
 const morgan = require('morgan');  // log requests to the console (express4)
 const bodyParser = require('body-parser'); // pull information from HTML POST (express4)
 const path = require('path');
-// const mongoose = require('mongoose');  
-
 //stuff to be rendered will be looked up in views
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 //any source files listed in your templates will be looked up here
 app.use(express.static(path.join(__dirname, 'public')));
+
+//load db: mongodb and mongoose
+const mongoose = require ('mongoose');
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('mongoose connected');
+});
+
 
 
 app.get('/', (req, res) => {
--- models/models.js
+++ models/models.js
@@ -1,6 +1,13 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test');
 
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('we are connected');
+
+});
+
 var ShopsSchema = new mongoose.Schema({
   restaurant_name: String,
   logo: Boolean,
